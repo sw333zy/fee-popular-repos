c142a956fc2439ee3083b330aa8813c82f98bfd1
J Ridgeway <sw333zy@gmail.com>
2016-12-12 16:19:46 -0500

    filter finally working w/out errors

---
+++ repos/github.filter.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular.module('gh')
+  .filter('bypopularity', ByPopularity);
+
+  function ByPopularity(){
+
+    return function sortByPopularity(input) {
+      var pop = input.map(function getpop(repo){
+        repo.pop = (repo.stargazers_count) + (repo.forks * 2) + (repo.open_issues/2);
+        return repo;
+
+      });
+
+      pop = pop.sort(function sorting(a, b){
+        if(a.pop > b.pop) {
+          return -1;
+        }
+        else{
+          return 1;
+        }
+
+
+      });
+      console.log(pop);
+      return pop;
+    };
+
+  }
+
+
+
+
+}());
